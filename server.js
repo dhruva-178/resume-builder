e465380f4ae4949da2a50bbd6e0f5d85f7061c16
Dhruva <mhatredhruva17@gmail.com>
2026-03-31 13:03:02 +0530

    fixed server error

--- server.js
+++ server.js
@@ -78,4 +78,8 @@ async function loadResumes() {
   }
 }
 
-window.onload = loadResumes;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
